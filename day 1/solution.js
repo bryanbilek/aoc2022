2678bd82d6ada313c0f6367383103dc15f61cb99
bryan <bbilek35@gmail.com>
2022-12-01 22:24:04 -0600

    day 1

---
+++ day 1/solution.js
@@ -0,0 +1,27 @@
+// Importing the fs module
+fs = require("fs")
+
+// Intitializing the readFileLines with filename
+fs.readFile('input.txt', function(err, data) {
+    if(err) throw err;
+
+    let array = data.toString().split('\n');
+    let mapped = array.map(item => item === '' ? ' ' : Number(item));
+    
+    let sums = [];
+    let curTotal = 0;
+    for (let item of mapped) {
+        if (item !== ' ') {
+            curTotal += item;
+        } else {
+            sums.push(curTotal);
+            curTotal = 0;
+        }
+    }
+    //Part One Solution
+    return Math.max(...sums);
+
+    //Part Two Solution
+    // let sorted = sums.sort((a,b) => b-a);
+    // return sorted[0] + sorted[1] + sorted[2];
+});
