55a6b6b090174a53244a65921142271c8cd04813
bryan <bbilek35@gmail.com>
2022-12-01 23:52:28 -0600

    day 2

--- day 2/solution.js
+++ day 2/solution.js
@@ -16,34 +16,45 @@ fs.readFile('day 2/input.txt', function(err, data) {
         let p2 = array[i][2];
 
         if (p1 === 'A' && p2 === 'X') {
-            sum += outcomes['draw'];
-            sum += scores['X'];
+            // sum += outcomes['draw'];
+            // sum += scores['X'];
+            sum += scores['C'];
         } else if (p1 === 'B' && p2 === 'Y') {
             sum += outcomes['draw'];
             sum += scores['Y'];
         } else if (p1 === 'C' && p2 === 'Z') {
-            sum += outcomes['draw'];
-            sum += scores['Z'];
-        } else if (p1 === 'A' && p2 === 'Y') {
+            // sum += outcomes['draw'];
             sum += outcomes['win'];
-            sum += scores['Y'];
+            // sum += scores['Z'];
+            sum += scores['X'];
+        } else if (p1 === 'A' && p2 === 'Y') {
+            // sum += outcomes['win'];
+            sum += outcomes['draw'];
+            // sum += scores['Y'];
+            sum += scores['A'];
         } else if (p1 === 'A' && p2 === 'Z') {
-            sum += scores['Z'];
+            // sum += scores['Z'];
+            sum += scores['B'];
+            sum += outcomes['win'];//part 2
         } else if (p1 === 'B' && p2 === 'X') {
             sum += scores['X'];
         } else if (p1 === 'B' && p2 === 'Z') {
             sum += outcomes['win'];
             sum += scores['Z'];
         } else if (p1 === 'C' && p2 === 'X') {
-            sum += outcomes['win'];
-            sum += scores['X'];
+            // sum += outcomes['win'];
+            // sum += scores['X'];
+            sum += scores['B'];
         } else if (p1 === 'C' && p2 === 'Y') {
-            sum += scores['Y'];
+            // sum += scores['Y'];
+            sum += scores['C'];
+            sum += outcomes['draw'] //part 2
         }
         
         //Part One Solution
-        return sum;
+        // return sum;
 
         //Part Two Solution
+        return sum;     
     }
 });
